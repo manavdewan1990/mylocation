77e18b82ea7974624772497d784c620292af2cd1
manavd <manavd@hotstar.com>
2018-10-02 17:30:17 +0530

    First commit

---
+++ src/main/java/com/mylocation/mylocation/MylocationApplication.java
@@ -0,0 +1,12 @@
+package com.mylocation.mylocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylocationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mylocation/mylocation/MylocationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mylocation.mylocation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MylocationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
